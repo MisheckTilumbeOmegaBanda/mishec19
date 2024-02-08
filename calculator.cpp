3348ea9a43779d87c80617f7d42ac18991d603c7
MisheckTilumbeOmegaBanda <159474800+MisheckTilumbeOmegaBanda@users.noreply.github.com>
2024-02-08 20:31:58 +0200

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
-#include "calculator.h"
+
 using namespace std;
 
 int main(){
 	
-	double angle;
-	cout << "Enter angle value:";
-	cin >> angle;
-	
-	trig cal(angle);
-	
-	cout << "\nThe answer is " << cal.findSum() << endl;
+	cout << "Hello World" << endl;
 }
